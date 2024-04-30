24ad9a67018c3ba3e166afba05ab33d8324f668c
omarkhalil117 <omarkhalil117@gmail.com>
2024-04-30 14:17:50 +0300

    link posts table with users table

--- database/migrations/2024_04_30_110219_create_posts_table.php
+++ database/migrations/2024_04_30_110219_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('body');
             $table->string('image')->nullable();
+            $table->foreignId('creator_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
